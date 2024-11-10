1a45d5b6825150b16727dd1c8344d1745123e5c0
OppRov <danielspeaker180@gmail.com>
2024-11-10 13:39:50 +0200

    test1

--- BackEnd/api/Controllers/UserController.js
+++ BackEnd/api/Controllers/UserController.js
@@ -2,17 +2,7 @@ const bcrypt = require('bcrypt')
 const mongoose = require('mongoose')
 
 
-exports.signup = async (req, res) => {
-    try {
-        const username = req.body.username
-        const email = req.body.email
-        const password = req.body.password
 
-
-    } catch {
-        res.status(500).send()
-    }
-}
 
 exports.login = async (req, res) => {
     try {
