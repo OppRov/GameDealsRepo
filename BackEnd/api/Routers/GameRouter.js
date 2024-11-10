140eecf53116dda8c06d514a0cda3977bc2449c3
OppRov <danielspeaker180@gmail.com>
2024-11-10 09:58:42 +0200

    Refactored both methods with Router and Controller files

--- BackEnd/api/Routers/GameRouter.js
+++ BackEnd/api/Routers/GameRouter.js
@@ -1,6 +1,10 @@
 const express = require('express')
-const app = express()
+const Router = express.Router()
 
 const Controller = require('../Controllers/GameController')
 
-app.get('/search/:title', Controller.search_for_game)
+Router.get('/search/:title', Controller.search_for_game)
+Router.get('/CurrentDeals/:id', Controller.displayAllDeals)
+
+
+module.exports = Router
--- BackEnd/server.js
+++ BackEnd/server.js
@@ -7,44 +7,9 @@ const port = 3000
 
 const GameRouter = require('./api/Routers/GameRouter')
 
-//app.use('/', GameRouter)
+app.use('/', GameRouter)
 
-// app.get('/search/:title', async (req, res) => {
-//     try {
-//         const title = req.params.title
-//         // console.log(title);
-//         const response = await fetch(`https://www.cheapshark.com/api/1.0/games?title=${title}`)
-//         const data = await response.json()
-//         //console.log(data[0]);
-//         res.json(data)
-//     } catch (error) {
-//         res.status(500).json(error)
-//     }
-// })
 
-app.get('/CurrentDeals/:id', async (req, res) => {
-    try {
-        const id = req.params.id
-        const data = await fetch(`https://www.cheapshark.com/api/1.0/games?id=${id}`)
-        const dealData = await data.json()
-        const deals = dealData.deals
-        const storeData = await fetch('https://www.cheapshark.com/api/1.0/stores')
-        const stores = await storeData.json()
-
-        const newData = deals.map(deal => {
-            const store = stores.find(store => store.storeID === deal.storeID)
-            return {
-                ...deal,
-                store
-            }
-        })
-
-        newData.unshift(dealData.cheapestPriceEver)
-        res.status(200).json(newData)
-    } catch (error) {
-        res.status(500).json(error)
-    }
-})
 
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
