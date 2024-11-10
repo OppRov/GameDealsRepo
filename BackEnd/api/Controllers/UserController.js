8b97418b40e2331bfdd1d6463dbee44ce2017322
OppRov <danielspeaker180@gmail.com>
2024-11-10 13:29:16 +0200

    began work on signup function

--- BackEnd/api/Controllers/UserController.js
+++ BackEnd/api/Controllers/UserController.js
@@ -1,5 +1,18 @@
 const bcrypt = require('bcrypt')
+const mongoose = require('mongoose')
 
+
+exports.signup = async (req, res) => {
+    try {
+        const username = req.body.username
+        const email = req.body.email
+        const password = req.body.password
+
+
+    } catch {
+        res.status(500).send()
+    }
+}
 
 exports.login = async (req, res) => {
     try {
